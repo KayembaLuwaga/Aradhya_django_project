331e418e2b8f9426c14ac55492a8df01e17a05a3
Kayemba Luwaga <76554604+KayembaLuwaga@users.noreply.github.com>
2024-02-14 04:22:33 +0000

    Update settings.py, db.sqlite3 config
    db.sqlite3 config

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-# import dj_database_url
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -94,7 +94,8 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
@@ -106,6 +107,7 @@ DATABASES = {
     # }
 }
 
+DATABASES['default'] = dj_database_url.config(default="sqlite:////os.path.join(BASE_DIR, 'db.sqlite3')")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -169,5 +171,5 @@ EMAIL_USE_TLS = True
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
