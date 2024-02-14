42c88e00470c75c31f7d961bf661d3889ee6f5bf
Kayemba Luwaga <76554604+KayembaLuwaga@users.noreply.github.com>
2024-02-14 13:36:08 +0000

    Update settings.py "sqlite:////os.path.join(BASE_DIR, 'db.sqlite3')"

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
 }
 
 DATABASES['default'] = dj_database_url.config(default="sqlite:////os.path.join(BASE_DIR, 'db.sqlite3')")
-DATABASE_URL = os.environ.get('DATABASE_URL', DATABASES['default'])
+DATABASE_URL = os.environ.get('DATABASE_URL', os.path.join(BASE_DIR, 'db.sqlite3'))
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
